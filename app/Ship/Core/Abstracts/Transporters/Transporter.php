68428f0d65d4d5e53e28014d4b4d4c3eac5ddf4a
Zair <nurmukhamadov.z@future-group.ru>
2021-08-03 00:37:32 +0600

    dto & pretus

--- app/Ship/Core/Abstracts/Transporters/Transporter.php
+++ app/Ship/Core/Abstracts/Transporters/Transporter.php
@@ -1,12 +1,42 @@
 <?php
 
-
 namespace App\Ship\Core\Abstracts\Transporters;
 
+use ReflectionClass;
+use ReflectionProperty;
+use Spatie\DataTransferObject\DataTransferObject;
 
-use Dto\Dto;
-
-abstract class Transporter extends Dto
+abstract class Transporter extends DataTransferObject
 {
+    public static function convert(array $data): DataTransferObject
+    {
+        $reflectionDto = new ReflectionClass(static::class);
+        $transformedProperties = [];
 
+        foreach ($reflectionDto->getProperties() as $property) {
+            if ($property->isPublic()) {
+                $name = $property->getName();
+                $type = $property->getType()->getName();
+                settype($data[$name], $type);
+
+                $transformedProperties[$name] = self::dataOrNull($property, $data[$name]);
+            }
+        }
+
+        return new static($transformedProperties);
+    }
+
+    /**
+     * @param ReflectionProperty $property
+     * @param mixed $data
+     * @return mixed|null
+     */
+    private static function dataOrNull(ReflectionProperty $property, $data)
+    {
+        if ($property->getType()->allowsNull() && empty($data)) {
+            return null;
+        }
+
+        return $data;
+    }
 }
